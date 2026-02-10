bef1ff08f1178c60b5e1cc6ee55e077e4d8ff4ef
Chaosok <kkongchaosok@gmail.com>
2026-02-10 13:47:49 +0100

    lier les pages ensembles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// 1. On importe les composants qu'on veut utiliser
+// On importe les composants 
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // 2. On les ajoute dans la liste des imports ICI :
+  // On les ajoute dans la liste des imports :
   imports: [RouterOutlet, NavbarComponent, FooterComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // 1. On importe le module commun
+import { CommonModule } from '@angular/common'; // On importe le module commun
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  // 2. On l'ajoute ici pour avoir le droit d'utiliser *ngFor
+  // pour avoir le droit d'utiliser *ngFor
   imports: [CommonModule], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  // Pas besoin de logique complexe pour l'instant
 }
