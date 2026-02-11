f9a463f345b8e29d832a7d7fc2d407e68c200d01
Nasro <Nasreddine.Biya.Etu@univ-lemans.fr>
2026-02-11 11:55:09 +0100

    Page Erreur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
+import { ErreurComponent } from './erreur/erreur.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },           // Page d'accueil par défaut
   { path: 'login', component: LoginComponent },     // Lien /login
   { path: 'register', component: RegisterComponent }, // Lien /register
   { path: 'contact', component: ContactComponent },   // Lien /contact
-  { path: '**', redirectTo: '' }                    // Si l'URL n'existe pas, retour accueil
+  { path: '**', component: ErreurComponent }          // Si l'URL n'existe pas, retour accueil
 ];
