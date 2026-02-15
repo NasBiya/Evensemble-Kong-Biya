0c69f05bfe42db0f2cbe0305867e8109f01f2c9c
Chaosok <kkongchaosok@gmail.com>
2026-02-15 20:43:01 +0100

    Page pricipal apres la connexion reussite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
 import { ErreurComponent } from './erreur/erreur.component';
+import { CarteComponent } from './carte/carte.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },           // Page d'accueil par défaut
   { path: 'login', component: LoginComponent },     // Lien /login
   { path: 'register', component: RegisterComponent }, // Lien /register
   { path: 'contact', component: ContactComponent },   // Lien /contact
+  { path: 'carte', component: CarteComponent },
   { path: '**', component: ErreurComponent }          // Si l'URL n'existe pas, retour accueil
 ];
