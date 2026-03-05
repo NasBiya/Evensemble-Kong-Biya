c78b2ebe99f555fde77909ececd19f2b82b01dd6
Nasro <Nasreddine.Biya.Etu@univ-lemans.fr>
2026-03-05 10:08:11 +0100

    Created an authentication guard that protects the routes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,16 +8,17 @@ import { CarteComponent } from './carte/carte.component';
 import { ProfilComponent } from './profil/profil.component';
 import { FavorisComponent } from './favoris/favoris.component';   
 import { ParametresComponent } from './parametres/parametres.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },           // Page d'accueil par défaut
   { path: 'login', component: LoginComponent },     // Lien /login
   { path: 'register', component: RegisterComponent }, // Lien /register
   { path: 'contact', component: ContactComponent },   // Lien /contact
-  { path: 'carte', component: CarteComponent },
-  { path: 'profil', component: ProfilComponent},
-  { path: 'favoris', component: FavorisComponent },
-  { path: 'parametres', component: ParametresComponent },
+  { path: 'carte', component: CarteComponent, canActivate: [AuthGuard] },
+  { path: 'profil', component: ProfilComponent, canActivate: [AuthGuard] },
+  { path: 'favoris', component: FavorisComponent, canActivate: [AuthGuard] },
+  { path: 'parametres', component: ParametresComponent, canActivate: [AuthGuard] },
   { path: '**', component: ErreurComponent }     // Si l'URL n'existe pas, retour accueil
 
 ];
