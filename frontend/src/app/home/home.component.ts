0f3176d408c8d1d99c16761cd2949eb3911ac618
Nasro <Nasreddine.Biya.Etu@univ-lemans.fr>
2026-03-05 10:12:14 +0100

    Modified the home page for logged in Users

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // On importe le module commun
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +11,13 @@ import { CommonModule } from '@angular/common'; // On importe le module commun
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Si l'utilisateur est connecté, rediriger vers la carte
+    if (this.authService.estConnecte()) {
+      this.router.navigate(['/carte']);
+    }
+  }
 }
